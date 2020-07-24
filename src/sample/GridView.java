7457084dd0dadd5fceee3a4ba0a8cf00a8f3d7c9
pabrodez <pabrodez@gmail.com>
2020-07-24 11:12:55 +0100

    Start win method

--- src/sample/GridView.java
+++ src/sample/GridView.java
@@ -6,6 +6,7 @@ import javafx.scene.layout.GridPane;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.stream.Stream;
 
@@ -14,27 +15,28 @@ import java.awt.*;
 public class GridView {
 
   private String turn;
-  private String[] movesArray = new String[9];
+  private String[] movesArray;
+
 
   public GridView() {
     this.turn = "X";
+    this.movesArray = new String[9];
+    Arrays.fill(movesArray, "");
   }
 
   private void changeTurn() {
+
     turn = (turn.equals("X")) ? "Y" : "X";
   }
 
   private boolean isWinner() {
-    boolean horizontal = (movesArray[0].equals(movesArray[1]) && movesArray[1].equals(movesArray[2])) ||
-            (movesArray[3].equals(movesArray[4]) && movesArray[4].equals(movesArray[5])) ||
-            (movesArray[6].equals(movesArray[7]) && movesArray[7].equals(movesArray[8]));
-    boolean vertical = (movesArray[0].equals(movesArray[3]) && movesArray[3].equals(movesArray[6])) ||
-            (movesArray[1].equals(movesArray[4]) && movesArray[1].equals(movesArray[7])) ||
-            (movesArray[2].equals(movesArray[5]) && movesArray[2].equals(movesArray[8]));
-    boolean diagonal = (movesArray[0].equals(movesArray[4]) && movesArray[4].equals(movesArray[8])) ||
-            (movesArray[2].equals(movesArray[4]) && movesArray[4].equals(movesArray[6]));
+    int[][] horizontalArrays = {{0, 1, 2}, {3, 4, 5}, {6, 7, 8}};
+    int[][] verticalArrays =   {{0, 3, 6}, {1, 4, 7}, {2, 5, 8}};
+    int[][] diagonalArrays =   {{0, 4, 8}, {2, 4, 6}};
+    boolean horizontal = Arrays.stream(horizontalArrays).anyMatch((arr) -> {
+      return (movesArray[arr[0]].matches("[XY]") && movesArray[arr[0]].equals(movesArray[arr[1]]) && movesArray[arr[0]].equals(movesArray[arr[2]]));
+    });
 
-    return horizontal || vertical || diagonal;
   }
 
   private Button createGridButton(int index) {
@@ -55,15 +57,35 @@ public class GridView {
   public Parent getView() {
     GridPane layout = new GridPane();
     Label turnLabel = new Label(turn);
-    layout.add(createGridButton(0), 0, 0);
-    layout.add(createGridButton(1), 1, 0);
-    layout.add(createGridButton(2), 2, 0);
-    layout.add(createGridButton(3), 0, 1);
-    layout.add(createGridButton(4), 1, 1);
-    layout.add(createGridButton(5), 2, 1);
-    layout.add(createGridButton(6), 0, 2);
-    layout.add(createGridButton(7), 1, 2);
-    layout.add(createGridButton(8), 2, 2);
+    ArrayList<Button> gridButtons = new ArrayList<>();
+    for (int i = 0; i <= 8; i++) {
+      Button gridButton = new Button("");
+      int index = i;
+      gridButton.setOnAction((event) -> {
+        // check squared is not already played
+        if (gridButton.getText().isBlank()) {
+          gridButton.setText(turn);
+          movesArray[index] = turn;
+          // check winner
+          if (isWinner()) {
+            turnLabel.setText("Winner is: " + this.turn);
+             return;
+          }
+          changeTurn();
+          turnLabel.setText(this.turn);
+        }
+      });
+      gridButtons.add(gridButton);
+    }
+    layout.add(gridButtons.get(0), 0, 0);
+    layout.add(gridButtons.get(1), 1, 0);
+    layout.add(gridButtons.get(2), 2, 0);
+    layout.add(gridButtons.get(3), 0, 1);
+    layout.add(gridButtons.get(4), 1, 1);
+    layout.add(gridButtons.get(5), 2, 1);
+    layout.add(gridButtons.get(6), 0, 2);
+    layout.add(gridButtons.get(7), 1, 2);
+    layout.add(gridButtons.get(8), 2, 2);
     layout.add(turnLabel, 0, 3);
 
     return layout;
